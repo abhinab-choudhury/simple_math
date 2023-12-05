2e8b416d41077f31de5a4e89f454bb2e0dda1b86
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2023-12-06 03:37:46 +0530

    Mod Function Added

--- index.js
+++ index.js
@@ -1,52 +1,72 @@
 const http = require('http');
 const url = require('url');
-const fs = require('fs');
 
 function serverHandler(req, res) {
-
-    const myUrl = url.parse(req.url, true); 
-    console.log(myUrl.pathname);
-
-    try {
-        let a, b;
-        if(myUrl.pathname !== '/favicon.ico') {
-            if(Number(myUrl.query.num1) && Number(myUrl.query.num2)) {
-                switch(myUrl.pathname) {
-                    case '/add':
-                        // multiplying to 1 automatically converts 
-                        // the type to int/float
-                        a = myUrl.query.num1 * 1; 
-                        b = myUrl.query.num2 * 1;
-                        res.end("... :) Hello From Simple Math API :) ..." + "\nNumber 1 : " + a + "\nNumber 2 : " + b + "\nSum : " + a + " + " + b  + " = " + (a + b)); 
-                        break;
-                    case '/sub':
-                        a = myUrl.query.num1 * 1;
-                        b = myUrl.query.num2 * 1;
-                        res.end("... :) Hello From Simple Math API :) ..." + "\nNumber 1 : " + a + "\nNumber 2 : " + b + "\nSub : " + a + " - " + b + " = " + (a - b));
-                        break;
-                   case '/mul':
-                        a = myUrl.query.num1 * 1;
-                        b = myUrl.query.num2 * 1;
-                        res.end("... :) Hello From Simple Math API :) ..." + "\nNumber 1 : " + a + "\nNumber 2 : " + b + "\nMul : " + a + " * " + b + " = " + (a * b));
-                        break;
-                    case '/div':
-                        a = myUrl.query.num1 * 1;
-                        b = myUrl.query.num2 * 1;
-                       res.end("... :) Hello From Simple Math API :) ..." + "\nNumber 1 : " + a + "\nNumber 2 : " + b + "\nDiv: " + a + " / " + b + " = " + parseFloat(a / b));
-                        break;
-                    default:
-                        res.end("404 Page Not Found");
-                }
-            } else {
-                res.end("Only Number and Float values are allowed.");
-            }
-        }
-    } catch(err) {
-        console.log(err);
+  const myUrl = url.parse(req.url, true);
+  try {
+    let a, b;
+    if (myUrl.pathname !== '/favicon.ico') {
+      switch (myUrl.pathname) {
+        case '/':
+          res.end("... :) Hello From Simple Math API :) ...");
+          break;
+        case '/add':
+          // multiplying to 1 automatically converts 
+          // the type to int/float
+          a = myUrl.query.num1 * 1;
+          b = myUrl.query.num2 * 1;
+          if(a && b) {
+            res.end("... :) Hello From Simple Math API :) ..." + "\nNumber 1 : " + a + "\nNumber 2 : " + b + "\nSum : " + a + " + " + b + " = " + (a + b));
+          } else {
+            res.end("Only int and Float Variables are allowed");
+          }
+          break;
+        case '/sub':
+          a = myUrl.query.num1 * 1;
+          b = myUrl.query.num2 * 1;
+          if(a && b) {
+            res.end("... :) Hello From Simple Math API :) ..." + "\nNumber 1 : " + a + "\nNumber 2 : " + b + "\nSub : " + a + " - " + b + " = " + (a - b));
+          } else {
+            res.end("Only int and Float Variables are allowed");
+          }
+          break;
+        case '/mul':
+          a = myUrl.query.num1 * 1;
+          b = myUrl.query.num2 * 1;
+          if(a && b) {
+            res.end("... :) Hello From Simple Math API :) ..." + "\nNumber 1 : " + a + "\nNumber 2 : " + b + "\nMul : " + a + " * " + b + " = " + (a * b));
+          } else {
+            res.end("Only int and Float Variables are allowed");
+          }
+          break;
+        case '/div':
+          a = myUrl.query.num1 * 1;
+          b = myUrl.query.num2 * 1;
+          if(a && b) {
+            res.end("... :) Hello From Simple Math API :) ..." + "\nNumber 1 : " + a + "\nNumber 2 : " + b + "\nDiv: " + a + " / " + b + " = " + parseFloat(a / b));
+          } else {
+            res.end("Only int and Float Variables are allowed");
+          }
+          break;
+        case '/mod':
+          a = myUrl.query.num1 * 1;
+          b = myUrl.query.num2 * 2;
+          if(a && b) {
+            res.end("... :) Hello From Simple Math API :) ..." + "\nNumber 1 : " + a + "\nNumber 2 : " + b + "\nDiv: " + a + " % " + b + " = " + parseFloat(a % b));
+          } else {
+            res.end("Only int and Float Variables are allowed");
+          }
+          break;
+        default:
+          res.end("404 Page Not Found");
+      }
     }
+  } catch (err) {
+    console.log(err);
+  }
 }
 const server = http.createServer((req, res) => serverHandler(req, res));
 
 server.listen(8080, () => {
-    console.log("Server Started!");
+  console.log("Server Started!");
 });
